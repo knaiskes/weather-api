1ada092a09690fb7b1342594d58d195fb16817c6
Kiriakos Naiskes <kiriakosnaiskes@gmail.com>
2022-02-23 20:32:56 +0200

    Add Sensor model

--- api/models.py
+++ api/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Sensor(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    room = models.CharField(max_length=50)
+    board = models.CharField(max_length=20)
+
+    def __str__(self):
+        return '%s - %s' % (self.name, self.board)
