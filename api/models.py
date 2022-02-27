581ef4ee086de3cdf82c7c5c6b4a812f91aa3856
Kiriakos Naiskes <kiriakosnaiskes@gmail.com>
2022-02-27 18:58:38 +0200

    Replace ids to uuids to Sensor and Metrics models

--- api/models.py
+++ api/models.py
@@ -1,6 +1,8 @@
 from django.db import models
+import uuid
 
 class Sensor(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=100, unique=True)
     room = models.CharField(max_length=50)
     board = models.CharField(max_length=20)
@@ -9,6 +11,7 @@ class Sensor(models.Model):
         return '%s - %s' % (self.name, self.board)
 
 class Metrics(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     temperature = models.FloatField()
     humidity = models.FloatField()
     sensor = models.ForeignKey(Sensor, on_delete=models.CASCADE)
