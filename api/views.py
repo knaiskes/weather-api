ae316f1a5bbc66df36217c9f1d48591aab923c66
Kiriakos Naiskes <kiriakosnaiskes@gmail.com>
2022-02-27 13:49:14 +0200

    Add api view to list all sensors

--- api/views.py
+++ api/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from api.models import Sensor
+from api.serializers import SensorSerializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+class SensorList(APIView):
+    '''
+    List all sensors
+    '''
+    def get(self, request, format=None):
+        sensors = Sensor.objects.all()
+        serializer = SensorSerializer(sensors, many=True)
+        return Response(serializer.data)
