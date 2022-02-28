6e675885114f50067daf493517b2c15bd68005a4
Kiriakos Naiskes <kiriakosnaiskes@gmail.com>
2022-02-28 23:02:23 +0200

    Add MetricsTestCase - test metrics str model

--- api/tests/test_models.py
+++ api/tests/test_models.py
@@ -7,3 +7,13 @@ class SensorTestCase(TestData):
         sensor2 = Sensor.objects.get(name='Sensor2')
 
         self.assertEqual(str(sensor1), '%s - %s' % (sensor1.name, sensor1.board))
+        self.assertEqual(str(sensor2), '%s - %s' % (sensor2.name, sensor2.board))
+
+
+class MetricsTestCase(TestData):
+    def test_str(self):
+        metrics1 = Metrics.objects.get(id=self.metrics1.id)
+        metrics2 = Metrics.objects.get(id=self.metrics2.id)
+
+        self.assertEqual(str(metrics1), 'Metrics by %s sensor' % (metrics1.sensor))
+        self.assertEqual(str(metrics2), 'Metrics by %s sensor' % (metrics2.sensor))
