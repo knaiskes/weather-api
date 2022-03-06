105935a5e8725262e53f65d58f5ec99f5d60a246
Kiriakos Naiskes <kiriakosnaiskes@gmail.com>
2022-03-06 14:11:07 +0200

    Add tests for SensorList post requests

--- api/tests/test_views.py
+++ api/tests/test_views.py
@@ -3,6 +3,7 @@ from django.urls import reverse
 from rest_framework import status
 from api.models import Sensor, Metrics
 from api.serializers import SensorSerializer
+from rest_framework.test import APIClient
 
 class SensorListTest(TestData):
     def test_get_all_sensors(self):
@@ -11,3 +12,18 @@ class SensorListTest(TestData):
         serializer = SensorSerializer(sensors, many=True)
         response = self.client.get(url, format='json')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_post_request(self):
+        client = APIClient()
+        url = reverse('sensor_list')
+        response = client.post(url, {'name': 'Sensor3', 'room': 'Living Room', 'board': 'Esp32'}, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_post_sensor_bad_request(self):
+        '''
+        Violate unique name field constraint
+        '''
+        client = APIClient()
+        url = reverse('sensor_list')
+        response = client.post(url, {'name': 'Sensor1', 'room': 'Different Room', 'board': 'Esp32'}, format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
