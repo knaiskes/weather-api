5ee818e2137503160110320ce68824c012504ce6
Kiriakos Naiskes <kiriakosnaiskes@gmail.com>
2022-03-07 13:09:01 +0200

    Add put method in SensorDetail view

--- api/views.py
+++ api/views.py
@@ -31,8 +31,15 @@ class SensorDetail(APIView):
         except Sensor.DoesNotExist:
             raise Http404
 
-
     def get(self, request, pk, format=None):
         sensor = self.get_object(pk)
         serializer = SensorSerializer(sensor)
         return Response(serializer.data)
+
+    def put(self, request, pk, format=None):
+        sensor = self.get_object(pk)
+        serializer = SensorSerializer(sensor, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
