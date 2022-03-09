1853ab170487ce8f4c73e1f9f3ddfad5162a23bd
Kiriakos Naiskes <kiriakosnaiskes@gmail.com>
2022-03-09 15:34:05 +0200

    Add a base user and token for testing

--- api/tests/base_tests.py
+++ api/tests/base_tests.py
@@ -1,4 +1,6 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from rest_framework.authtoken.models import Token
 from api.models import Sensor, Metrics
 
 class TestData(TestCase):
@@ -11,3 +13,9 @@ class TestData(TestCase):
         # Metrics
         cls.metrics1 = Metrics.objects.create(temperature=18.3, humidity=40.32, sensor=cls.sensor1)
         cls.metrics2 = Metrics.objects.create(temperature=20.9, humidity=60.10, sensor=cls.sensor2)
+
+        # Users
+        cls.user1 = User.objects.create_user('testUser', 'testUser@test.com', 'testPass')
+
+        # Tokens
+        cls.token1, _ = Token.objects.get_or_create(user=cls.user1)
